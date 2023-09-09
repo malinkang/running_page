06012b8f51ec4a9aac2a055fa60386a99d5b87a8
malinkang <malinkang@adsconflux.com>
2023-09-09 20:26:45 +0800

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteUrl: 'https://run.malinkang.com',
+    logo: 'https://avatars.githubusercontent.com/u/3365208?v=4',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://blog.malinkang.com',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://malinkang.com',
       },
     ],
   },
